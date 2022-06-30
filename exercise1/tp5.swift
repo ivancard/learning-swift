f99c63305bc9820bfdb5ccec93e0cbeb478d423e
Ivan <78120099+ivancard@users.noreply.github.com>
2022-06-30 00:48:32 -0300

    Update tp5.swift

--- exercise1/tp5.swift
+++ exercise1/tp5.swift
@@ -1,3 +1,22 @@
+import UIKit
+
+enum requiredSpecifications: String  {
+    case numbers = "0123456789"
+    case lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    case upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    case specialCharacters = "!@#$%^&*()-+."
+}
+
+func hasARequiredSpecification(type specificated: requiredSpecifications, evaluate chain: String) -> Bool {
+    let arrOfSpectificated = Array(specificated.rawValue)
+    for char in chain {
+        if arrOfSpectificated.contains(char){
+            return true
+        }
+    }
+    return false
+}
+
 class passwordValidator {
 
     let password: String
@@ -6,24 +25,27 @@ class passwordValidator {
         self.password = password
     }
 
-    func validate(password: String) -> String {
-        let numbers = "0123456789"
-        let lower_case = "abcdefghijklmnopqrstuvwxyz"
-        let upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        let special_characters = "!@#$%^&*()-+"
-
-        if  password.count >= 6 &&
-            password.contains(lower_case) &&
-            password.contains(upper_case) &&
-            password.contains(numbers) &&
-            password.contains(special_characters) {
-            return "The password is strong"
-        }else{
-            return "The password is not strong"
-        }
+    func validate() -> String {
+        
+        var errorMesagge = "Your password must contains: almost "
+        
+        let hasANumber = hasARequiredSpecification(type: .numbers, evaluate: self.password)
+       
+        let hasALowerCase = hasARequiredSpecification(type: .lowerCase, evaluate: self.password)
+        
+        let hasAUpperCase = hasARequiredSpecification(type: .upperCase, evaluate: self.password)
+        
+        let hasASpecialCharacter = hasARequiredSpecification(type: .specialCharacters, evaluate: self.password)
+        
+        
+        errorMesagge += hasANumber == false ? "1 number " : ""
+        errorMesagge += hasALowerCase == false ? "1 lower case " : ""
+        errorMesagge += hasAUpperCase == false ? "1 upper case " : ""
+        errorMesagge += hasASpecialCharacter == false ? "1 special character " : ""
+        
+        return hasANumber && hasALowerCase && hasAUpperCase && hasASpecialCharacter ? "is strong" : errorMesagge
     }
-
 }
 
-var password = passwordValidator(password: "123456")
-print(password.validate())
+var pass = passwordValidator(password: "aA")
+print(pass.validate())
